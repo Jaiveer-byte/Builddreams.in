7616310c1d5dee77419edafb5ee89623d191ec96
Jaiveer-byte <jaiveerchauhan01@gmail.com>
2026-06-20 16:21:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfSDtAXtV8xCiOJM52kkCuw4uQu-pSZJI",
+  authDomain: "construction-bazar-206d8.firebaseapp.com",
+  projectId: "construction-bazar-206d8",
+  storageBucket: "construction-bazar-206d8.firebasestorage.app",
+  messagingSenderId: "59747288512",
+  appId: "1:59747288512:web:3b0178c9b7472bc7f7f425"
+}
